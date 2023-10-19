59568d8b6bd97e112f1bd1b103caf3ceaa463002
oerett <eretosmenaj8@gmail.com>
2023-10-19 19:52:52 +0200

    in rogress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './menus/login/login.component';
 import { RegisterComponent } from './menus/register/register.component';
+import { Routes } from '@angular/router';
+
+
+const routes: Routes = [
+  {
+    path: 'login',
+    loadChildren: () => import('./menus/login/login.module').then(m => m.LoginModule)
+  },
+  // { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   declarations: [
