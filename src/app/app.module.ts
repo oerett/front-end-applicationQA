816e6cd1af22f0dbd6b52f055f84e95135e76370
oerett <eretosmenaj8@gmail.com>
2023-10-20 18:32:51 +0200

    cache disabled

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './menus/login/login.component';
-import { RegisterComponent } from './menus/register/register.component';
+import { LoginComponent } from './authentication/login/login.component';
+import { RegisterComponent } from './authentication/register/register.component';
 import { Routes } from '@angular/router';
 
 
 const routes: Routes = [
-  {
-    path: 'login',
-    loadChildren: () => import('./menus/login/login.module').then(m => m.LoginModule)
-  },
+  // {
+  //   path: 'login',
+  //   loadChildren: () => import('./authentication/login/login.module').then(m => m.LoginModule)
+  // },
   // { path: '**', redirectTo: '' }
 ];
 
